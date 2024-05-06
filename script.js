9ad0079fce140b00c606abce62a203b2c0efee0f
Larysa Rud <larpavrud@gmail.com>
2024-05-06 11:37:53 -0400

    Adding the roll method to the Character class.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ adventurer.inventory.forEach((item) => {
 adventurer.companion.companion2 = {
     name: "Frank",
     type: "Flea",
-    belongings: ["hat", "sunglasses"],
+    inventory: ["small hat", "sunglasses"],
 };
 console.log(adventurer);
 //Test the “dice rolls” method by calling adventurer.roll():
@@ -29,13 +29,18 @@ adventurer.roll();
 
 //Part 2: Class Fantasy
 
-    /* Basic Character class looks like so far, 
-    including a constructor function that allows us to create new characters 
-    with whatever name we would like: */
-    class Character {
-        constructor (name) {
-          this.name = name;
-          this.health = 100;
-          this.inventory = [];
-        }
+/* Basic Character class looks like so far, 
+including a constructor function that allows us to create new characters 
+with whatever name we would like: */
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
     }
+    //Every character should also be able to make rolls. Add the roll method to the Character class:
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
