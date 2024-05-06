43220f18aece4b5a760b3a878fd32609d6d1deb4
Larysa Rud <larpavrud@gmail.com>
2024-05-06 10:49:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Part 1: Humble Beginnings
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+      name: "Leo",
+      type: "Cat",
+    },
+}
